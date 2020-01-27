83ce1ebf0b3de45cc7bcf14844ffb9ebb52d5515
Jaime Tenorio Ballesteros <jaime.tenorio@stkprojects.com>
2020-01-27 11:59:14 -0600

    Style fixes

--- server/server.py
+++ server/server.py
@@ -53,10 +53,10 @@ def chat():
     return Response(status = 200)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(
-        host='0.0.0.0',
-        port='5000',
+        host="127.0.0.1",
+        port="5000",
         threaded=True,
         debug=True
     )
